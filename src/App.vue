51066e284be0fdcecdc43539cf1625e101cf7da9
Tim van Wolfswinkel <tim.vanwolfswinkel@afriendofmine.nl>
2018-02-10 14:08:24 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="links">
+      <router-link class="nav-link" active-class="active" to="/">home</router-link>
+      <router-link class="nav-link" active-class="active" to="/test">test</router-link>
+    </div>
+    <transition name="fade">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.links {
+  text-align: center;
+}
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+</style>
