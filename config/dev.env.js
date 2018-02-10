5e5dd50384a5a3bf4125405c68930f09e4cdacf1
Tim van Wolfswinkel <tim.vanwolfswinkel@afriendofmine.nl>
2018-02-10 17:43:27 +0000

    Moved access token to other file

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,8 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const apiEnv = require('./api.env')
 
-module.exports = merge(prodEnv, {
+module.exports = merge(prodEnv, apiEnv, {
   NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,4 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  API_URL: '"https://cdn.contentful.com"',
-  SPACE_ID: '"uxofjitlyd9u"',
-  ACCESS_TOKEN : '"b642267caad1e5d551c277edb09c89e27d1ffe808cf39cc4b951da667d9687fd"'
+  NODE_ENV: '"production"'
 }
